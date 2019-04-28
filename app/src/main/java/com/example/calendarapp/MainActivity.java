bedeecb4096937306a77af7380dcefba0c477ed2
natalielindquist <nkl3@illinois.edu>
2019-04-28 17:26:05 -0500

    save

--- app/src/main/java/com/example/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     CalendarView calendarView;
@@ -20,21 +22,19 @@ public class MainActivity extends AppCompatActivity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                String date = ((1 + month) + "/" + dayOfMonth + "/" + year + " is a birthday! Don't forget to send your wishes.");
+                String date = ((1 + month) + "/" + dayOfMonth + "/" + year + " is a birthday, don't forget to send your wishes!");
                 myDate.setText(date);
             }
         });
 
-        final Imagebutton setDate = findViewById(R.id.myDate);
-        setDate.setOnClickListener(v -> {
-            Log.d(TAG, "Who's birthday is this day?: ");
-            startSetDate();
-        });
-
-        final Imagebutton message = findViewById(R.id.sendMessage);
-        message.setOnClickListener(v -> {
-            Log.d(TAG, "Happy Birthday message sent :) ");
-            startmessage();
-        });
+//        Button setDate = findViewById(R.id.myDate);
+//        setDate.setOnClickListener(v -> {
+//
+//        });
+//
+//        Button message = findViewById(R.id.sendMessage);
+//        message.setOnClickListener(v -> {
+//
+//        });
     }
 }
