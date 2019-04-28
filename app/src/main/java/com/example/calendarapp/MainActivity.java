378b9210943f11fd1cb86e74cb547dbf2078ef1b
janetlee <janetl2@illinois.edu>
2019-04-28 14:55:46 -0500

    project

--- app/src/main/java/com/example/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.calendarapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView myDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = ((1 + month) + "/" + dayOfMonth + "/" + year + " is a birthday! Don't forget to send your wishes.");
+                myDate.setText(date);
+
+            }
+        });
     }
 }
